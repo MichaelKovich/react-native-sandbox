5746bac704b7cb99fb2e15e105f7cc28fa1ddda7
Michael Kovich <kovich@michaelkovich.com>
2018-04-25 09:56:42 -0500

    Functional, barebones application.

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text style={styles.welcomeText}>Michael's Really Cool Android Application</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
@@ -19,5 +19,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  welcomeText: {
+    fontSize: 20,
+    padding: 15,
+    backgroundColor: 'slategray',
+    marginBottom: '5%',
+    color: 'white',
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: 'black',
   },
 });
